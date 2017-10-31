1295fa14b8218e274977fef4835335c5313f2135
sonal18d <31699042+sonal18d@users.noreply.github.com>
2017-10-31 12:41:46 -0400

    Question topic properties added
    Question topic properties added

--- RRAstro.Core/Domain/QuestionAns/QuestionTopic.cs
+++ RRAstro.Core/Domain/QuestionAns/QuestionTopic.cs
@@ -11,6 +11,14 @@ namespace RRAstro.Core.Domain.QuestionAns
         {
             QuestionList = new List<Question>();
         }
+       public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public string email { get; set; }
+        public DateTime dateOfBirth { get; set; }
+        public DateTime timeOfBirth { get; set; }
+        public string birthCity { get; set; }
+        public string birthCountry { get; set; }
         public string UserID { get; set; }
         public ICollection<Question> QuestionList { get; set; }
     }
